d54cbdb97e9b7f5b600704c187161c1b82d53434
Nama' Salameh <nama.salameh@gmail.com>
2023-09-14 07:57:05 +0300

    Passed most of the tests

--- caculator.js
+++ caculator.js
@@ -1,6 +1,6 @@
 function calc(number1, operator, number2) {
   if (typeof number1 !== "number" || typeof number2 !== "number")
-    throw new Error("Invalid type");
+    throw new Error("Invalid input type");
 
   switch (operator) {
     case "+":
@@ -10,10 +10,12 @@ function calc(number1, operator, number2) {
     case "*":
       return number1 * number2;
     case "/": {
-      if (number2 === 0) throw new Error("Connot divide by zero");
+      if (number2 === 0) throw new Error("Division by zero");
       return number1 / number2;
     }
     default:
-      throw new Error("Invalid operatoe");
+      throw new Error("Invalid operator");
   }
 }
+
+module.exports = calc;
